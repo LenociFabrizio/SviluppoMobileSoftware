4919cf938cb3686cb33d228798b48902ed948899
IsteriEndrit <endritisteri@gmail.com>
2022-09-14 04:26:37 +0200

    collegamento del QRScanner con MostraOggettoDiInteresse(ancora non possiamo vedere se mostrare o meno le attività) e finire di collegare il Bluetooth(problema con le richieste di permessi da Service, Android non le implementa, vedere come risolvere..

---
+++ app/src/main/java/it/uniba/di/e_cultureexperience/Bluetooth/MainActivity.java
@@ -0,0 +1,94 @@
+package it.uniba.di.e_cultureexperience.Bluetooth;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.Toast;
+import it.uniba.di.e_cultureexperience.R;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    BluetoothAdapter bluetoothAdapter;
+    final int REQUEST_ENABLE_BT = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        // Use this check to determine whether Bluetooth classic is supported on the device.
+        // Then you can selectively disable BLE-related features.
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH) || bluetoothAdapter == null) {
+            Toast.makeText(this, "Bluetooth non supportato", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+        //ALLORA BT è disponibile, devo controllare se è attivo
+        if (!bluetoothAdapter.isEnabled()) {
+            if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.BLUETOOTH_CONNECT) == PackageManager.PERMISSION_GRANTED) {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            } else {
+                requestBluetoothConnectPermission();
+            }
+        } else {
+            //è abilitato e continuo con l' avvio del servizio
+            Intent serviceIntent = new Intent(this, BtService.class);
+            //non serve, con l' avvio del percorso setterò i dati dell' Application
+            //serviceIntent,putExtraData("");
+            startService(serviceIntent);
+        }
+    }
+
+    private void requestBluetoothConnectPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.BLUETOOTH_CONNECT)) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                new AlertDialog.Builder(this)
+                        .setTitle("Attivazione Bluetooth")
+                        .setMessage("Il Bluetooth serve per trovare gli oggetti di interessi nelle vicinanze.")
+                        .setPositiveButton("ok", (dialog, which) -> ActivityCompat.requestPermissions(this,
+                                new String[]{Manifest.permission.BLUETOOTH_CONNECT}, REQUEST_ENABLE_BT))
+                        .setNegativeButton("cancella", (dialog, which) -> dialog.dismiss())
+                        .create().show();
+            }
+        } else {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.BLUETOOTH_CONNECT}, REQUEST_ENABLE_BT);
+            }
+        }
+    }
+
+    @SuppressLint("MissingSuperCall")
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_ENABLE_BT) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permesso Concesso", Toast.LENGTH_SHORT).show();
+                Intent serviceIntent = new Intent(this, BtService.class);
+                startService(serviceIntent);
+            } else {
+                Toast.makeText(this, "Permesso Rifiutato", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
--- app/src/main/java/it/uniba/di/e_cultureexperience/QRScanner/QRScanner.java
+++ app/src/main/java/it/uniba/di/e_cultureexperience/QRScanner/QRScanner.java
@@ -1,15 +1,25 @@
 package it.uniba.di.e_cultureexperience.QRScanner;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
+import it.uniba.di.e_cultureexperience.OggettoDiInteresse.MostraOggettoDiInteresseActivity;
+import it.uniba.di.e_cultureexperience.OggettoDiInteresse.OggettoDiInteresse;
 import it.uniba.di.e_cultureexperience.Percorso.AvviaPercorsoActivity;
 import it.uniba.di.e_cultureexperience.R;
 
@@ -20,7 +30,7 @@ public class QRScanner extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qr_scanner);
         IntentIntegrator integrator = new IntentIntegrator(this);
-        integrator.setPrompt("For flash use volume up key");
+        integrator.setPrompt("Stringa per il flash");
         integrator.setOrientationLocked(true);
         integrator.setCaptureActivity(Capture.class);
         integrator.initiateScan();
@@ -34,23 +44,64 @@ public class QRScanner extends AppCompatActivity {
         if (result.getContents() != null )
         {
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            builder.setTitle("result");
+            builder.setCancelable(true);
+            builder.setTitle("Risultato Scansione");
             String contentScanned = result.getContents();
-            builder.setMessage(contentScanned);
 
-            builder.setPositiveButton("OK", (dialogInterface, i) -> {
-                String idOggettoDaScannerizzare = null;
-                if (contentScanned.equals(idOggettoDaScannerizzare)){
-                    Toast.makeText(getApplicationContext(), "Esatto, hai scannerizzato correttamente!", Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(QRScanner.this, AvviaPercorsoActivity.class);
-                    intent.putExtra("Visitato", true);
-                    startActivity(intent);
-                }else{
-                    Toast.makeText(getApplicationContext(), "Riprova scannerizzando il giusto oggetto!", Toast.LENGTH_SHORT).show();
+            //controllo se corrisponde al QR di un oggetto di interesse
+            FirebaseFirestore rootRef = FirebaseFirestore.getInstance();
+            DocumentReference docIdRef = rootRef.collection("oggetti").document(String.valueOf(contentScanned));
+            docIdRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                    if (task.isSuccessful()) {
+                        DocumentSnapshot document = task.getResult();
+                        if (document.exists()) {
+                            //CORRISPONDE al QR di un oggetto
+                            //CHIEDO ALL' UTENTE SE VUOLE VISUALIZZARLO
+                            builder.setMessage("Vuoi visualizzare l' oggetto scansionato?");
+                            builder.setPositiveButton("Conferma",
+                                    new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            OggettoDiInteresse oggettoDiInteresse = document.toObject(OggettoDiInteresse.class);
+                                            oggettoDiInteresse.setId(document.getId());
+                                            Intent intent = new Intent(QRScanner.this, MostraOggettoDiInteresseActivity.class);
+                                            intent.putExtra("oggettoDiInteresse", oggettoDiInteresse);
+                                            startActivity(intent);
+                                        }
+                                    });
+                            builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int which) {
+                                    //rimane alla scannerizzazione
+                                    //Toast.makeText(getApplicationContext(), "You didn' t scan anything", Toast.LENGTH_SHORT).show();
+                                    dialogInterface.dismiss();
+                                    finish();
+                                }
+                            });
+
+                            AlertDialog dialog = builder.create();
+                            dialog.show();
+                        } else {
+                            //NON CORRISPONDE al QR di un oggetto di interesse
+                            AlertDialog.Builder builder = new AlertDialog.Builder(QRScanner.this);
+
+                            builder.setMessage("Il QR Code scansionato  non corrisponde ad un Oggetto di Interesse");
+                            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    dialogInterface.dismiss();
+                                    finish();
+                                }
+                            });
+                            builder.show();
+                        }
+                    } else {
+                        Log.d("Endrit", "Failed with: ", task.getException());
+                    }
                 }
-                //mi porta dov ero
-                //Intent intent = new Intent(QRScanner.this, /*QUI*/DashboardMeteActivity.class);
-
+            });
                 //prendo l id preso
                 //se il percorso è avviato, controllo se corrisponde all' oggetto che deve trovare
                     //se non corrisponde all' oggetto da trovare ritorna all' activity del percorso
@@ -61,12 +112,6 @@ public class QRScanner extends AppCompatActivity {
                 //boolean loginGoogle = getIntent().getExtras().getBoolean("segnalino");
                 //immetto segnalino in dashboard mete
                 //i.putExtra("segnalino",loginGoogle);
-
-                /*startActivity(intent);
-                dialogInterface.dismiss();*/
-
-            });
-            builder.show();
         }else{
             Toast.makeText(getApplicationContext(), "You didn' t scan anything", Toast.LENGTH_SHORT).show();
         }
